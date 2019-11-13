8445db3ea9b8f7890882d784fc466ce2a606dacb
Rajdeep Bharati <rajdeep@macports.org>
2019-11-14 00:15:47 +0530

    Add vue carousel

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
   <div id="app">
-    <h1>GetFit</h1>
-    <div id="nav">
-      <router-link :to="{ name: 'home' }">Vue</router-link> |
-      <router-link :to="{ name: 'messages' }">Django Rest</router-link>
-    </div>
-    <router-view />
+    <template>
+      <b-navbar>
+        <template slot="brand">
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <!-- <img
+              src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+              alt="Lightweight UI components for Vue.js based on Bulma"
+            /> -->
+            <h1>VigorIt</h1>
+          </b-navbar-item>
+        </template>
+        <template slot="start">
+          <b-navbar-item href="#">
+            Home
+          </b-navbar-item>
+          <b-navbar-dropdown label="Info">
+            <b-navbar-item href="#">
+              About
+            </b-navbar-item>
+            <b-navbar-item href="#">
+              Contact
+            </b-navbar-item>
+          </b-navbar-dropdown>
+        </template>
+
+        <template slot="end">
+          <b-navbar-item tag="div">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+    </template>
+    <h1>VigorIt</h1>
+    <!-- <router-view /> -->
+    <carousel
+      :per-page="1"
+      :navigate-to="someLocalProperty"
+      :mouse-drag="false"
+    >
+      <slide @slideclick="handleSlideClick">
+        <img src="@/assets/logo-vue.png" alt="" />
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
   </div>
 </template>
 
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+
+export default {
+  components: {
+    Carousel,
+    Slide
+  },
+  methods: {
+    handleSlideClick () {}
+  }
+}
+</script>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
